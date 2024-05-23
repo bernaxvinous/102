29276bce1834536675fab93c1f9ee47e7839a61d
bernaxvinous <150352167+bernaxvinous@users.noreply.github.com>
2024-05-23 14:31:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//comece a programar aqui
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+    });
+
+    camera = document.getElementById("camera");
+
+    Webcam.attach( '#camera' )
+
+    function takeSnapshot()
+    {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+    }
+    console.log('versão ml5:', ml5.version);
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-mDpXJtc5/model.json',modelLoaded);
+
+
+
+
+
+
+
+
